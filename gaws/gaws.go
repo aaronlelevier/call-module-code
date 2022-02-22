c322c354cdc93e70377c2a803bbb50b6de7fb926
aaronlelevier <aaron.lelevier@gmail.com>
2022-02-22 07:33:34 -0800

    can join file paths

--- gaws/gaws.go
+++ gaws/gaws.go
@@ -3,13 +3,30 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
+	"path"
+	"path/filepath"
 )
 
-func main() {
+func readFile() string {
+	wd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	parent := filepath.Dir(wd)
+	return path.Join(parent, "gaws/tests/data/ec2/describe-vpcs.json")
+}
+
+func callAwsApi() string {
 	out, err := exec.Command("aws", "sts", "get-caller-identity").Output()
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("The date is %s\n", out)
+	return string(out)
+}
+
+func main() {
+	out := readFile()
+	fmt.Printf("%s\n", out)
 }
